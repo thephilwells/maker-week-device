b6bf2d8a4c0a72ed45144c27bafb43dff1a2111a
Phil Wells <phil.wells@nytimes.com>
2018-07-24 15:31:22 -0400

    calculates total possible score

--- device/device.ino
+++ device/device.ino
@@ -10,9 +10,21 @@ int lastButtonAState = HIGH;
 int lastButtonBState = HIGH;
 int lastButtonCState = HIGH;
 
-char allLetters[8]   = "TDMRABO";
+String outerLetters[7] = {"D","M","R","A","B","O"};
 
-String dictionary[48] = {"ABBOT","ABORT","AORTA","ATOM","ATTAR","BOAT","BOOT","BOTTOM","BRAT","DART","DARTBOARD","DATA","DOORMAT","DOTARD","DRAT","MARMOT","MART","MATADOR","MOAT","MOOT","MORTAR","MOTOR","MOTORBOAT","MOTTO","ORATOR","RATATAT","ROBOT","ROOT","ROTATOR","ROTO","ROTOR","TABARD","TABOO","TABOR","TARO","TAROT","TART","TARTAR","TATTOO","TOAD","TOMATO","TOMB","TOOT","TORO","TORT","TRAM","TROD","TROT"};
+String input[20];
+int possibleScore = 0;
+
+String centerLetter = "T";
+String northwestLetter = outerLetters[0];
+String northeastLetter = outerLetters[1];
+String eastLetter = outerLetters[2];
+String southeastLetter = outerLetters[3];
+String southwestLetter = outerLetters[4];
+String westLetter = outerLetters[5];
+
+const String dictionary[49] = {"ABBOT","ABORT","AORTA","ATOM","ATTAR","BOAT","BOOT","BOTTOM","BRAT","DART","DARTBOARD","DATA","DOORMAT","DOTARD","DRAT","MARMOT","MART","MATADOR","MOAT","MOOT","MORTAR","MOTOR","MOTORBOAT", "MORTARBOARD", "MOTTO","ORATOR","RATATAT","ROBOT","ROOT","ROTATOR","ROTO","ROTOR","TABARD","TABOO","TABOR","TARO","TAROT","TART","TARTAR","TATTOO","TOAD","TOMATO","TOMB","TOOT","TORO","TORT","TRAM","TROD","TROT"};
+String pangram = "MOTORBOAT";
 
 void setup()
 {
@@ -20,6 +32,7 @@ void setup()
   pinMode(buttonBpin, INPUT_PULLUP);
   pinMode(buttonCpin, INPUT_PULLUP);
   Serial.begin(9600);
+  calculateTotalPoints();
 }
 
 void loop() 
@@ -48,3 +61,28 @@ void loop()
       delay(50);
     }
 }
+
+void calculateTotalPoints()
+{
+  for (int i=0; i < 49; i++) {
+    String word = dictionary[i];
+    if (word == pangram)
+    {
+      possibleScore += word.length() + 7;
+      Serial.println("Added "+pangram+". New possible score: "+possibleScore);
+    }
+    else
+    {
+      if (word.length() == 4)
+      {
+        possibleScore += 1;
+      }
+      else
+      {
+        possibleScore += word.length();
+      }
+      Serial.println("Added "+word+". New possible score: "+possibleScore);
+    }
+  }
+}
+
